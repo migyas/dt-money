a9720ce17b1c656383d72a6c8deb8c1c591828c6
Yan Dias <yansantos0712@gmail.com>
2023-01-28 18:22:56 -0300

    feat: static application finished

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,19 @@
+import * as Dialog from "@radix-ui/react-dialog";
 import { Container, Content, NewTransactionButton } from "./styles";
 import logoImg from "../../assets/logo.svg";
+import { NewTransactionModal } from "../NewTransactionModal";
 
 export function Header() {
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="Logomarca do projeto" />
-        <NewTransactionButton>Nova transação</NewTransactionButton>
+        <Dialog.Root>
+          <Dialog.DialogTrigger asChild>
+            <NewTransactionButton>Nova transação</NewTransactionButton>
+          </Dialog.DialogTrigger>
+          <NewTransactionModal />
+        </Dialog.Root>
       </Content>
     </Container>
   );
